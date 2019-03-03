cc2debd66ad2f4866cab5785ad33fbba1b62ecbd
Kenit <Kenit@Kenit-PC>
2019-03-03 18:21:24 +0000

    Initial commit of the project

---
+++ gumtree/src/main/java/com/gumtree/customer/GumtreeApplication.java
@@ -0,0 +1,13 @@
+package com.gumtree.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumtreeApplication.class, args);
+	}
+
+}
